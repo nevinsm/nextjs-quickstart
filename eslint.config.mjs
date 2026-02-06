010359d17cd1da5ead5af8116994bec15536ba85
Nevin Morgan <nevin@longtowel.com>
2026-02-05 21:31:17 -0500

    chore(next): upgrade project to Next.js 16

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    ".eslintrc.cjs",
+    "postcss.config.cjs",
+    "tailwind.config.cjs",
+  ]),
+]);
+
+export default eslintConfig;
